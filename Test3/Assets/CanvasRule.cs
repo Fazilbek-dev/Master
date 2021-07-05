d1390badf232a0d65c0a975674698efb3f7cb726
Fazilbek-dev <chestniy843@gmail.com>
2021-07-05 17:47:05 +0500

    2019.4.26

--- Test3/Assets/CanvasRule.cs
+++ Test3/Assets/CanvasRule.cs
@@ -1,24 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CanvasRule : MonoBehaviour
 {
-
-    Touch touch;
-
-    Transform tr;
-
-    private void Start()
+    public void Restart()
     {
-        tr = GetComponent<Transform>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.touchCount > 0) transform.eulerAngles = new Vector3(0, 0, 20.1f);
-        var rotateNeed = Quaternion.LookRotation(Input.mousePosition - transform.position);
-        //tr.eulerAngles = Quaternion.Euler(0, 0, rotateNeed);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
---
+++ Test3/Assets/CarController2.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
+
+public class CarController2 : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float speed;
+    public float maxSteeringAngle;
+    public Transform mePos;
+    public Transform[] checkPoints;
+    public GameObject WheelRight;
+    public GameObject WheelLeft;
+    //public GameObject WheelR;
+    //public GameObject WheelL;
+    public bool BackWheels;
+    public bool BackFront;
+    public int WheelRotateAngle = 2;
+    public float MaxDistance = 2;
+
+    public int i = 0;
+
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void Update()
+    {
+
+        if (MaxDistance > Vector3.Distance(transform.position, checkPoints[i].position))
+        {// если MaxDistance больше дистанции до цели
+            i++;
+        }
+        if(i >= checkPoints.Length)
+        {
+            i = 0;
+        }
+        
+        var rotateNeed = Quaternion.LookRotation(checkPoints[i].transform.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotateNeed,10f * Time.deltaTime);
+
+
+        float motor = maxMotorTorque;
+
+        float steering = maxSteeringAngle * (speed + 0.7f);
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+}
